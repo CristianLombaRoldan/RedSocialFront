ce4b973a5b1f547b2ad9ee0f037849cb496d6316
Cristian Lomba Roldán <cristian.lomba@a.sevillasj.es>
2025-11-09 12:51:58 +0100

    navegacion con react router dom

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+// src/pages/HomePage.jsx
+import Header from "../components/Header";
+
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <main style={{ padding: 20 }}>
+        <h3>Publicaciones de tus seguidos</h3>
+    
+      </main>
+    </>
+  );
+}
