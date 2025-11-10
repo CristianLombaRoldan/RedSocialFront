7a19ee7f80767cdd8f0508035447b190e32efeb1
Cristian Lomba Roldán <cristian.lomba@a.sevillasj.es>
2025-11-10 16:45:34 +0100

    Visualizacion de perfiles de los usuarios

--- src/pages/MyProfilePage.jsx
+++ src/pages/MyProfilePage.jsx
@@ -1,19 +1,27 @@
 // src/pages/MyProfilePage.jsx
 import Header from "../components/Header";
-import MyPublication from "../components/MyPublications";
-
-
-
+import { useAuth } from "../context/useAuth";
+import MyPublications from "../components/MyPublications";
+import MyUserProfile from "../components/MyUserProfile";
 
 export default function MyProfilePage() {
+  const { user } = useAuth();
+
+  // Si aún no se ha cargado el user del contexto
+  if (!user) return <p>Cargando tu perfil...</p>;
+
   return (
     <>
       <Header />
       <main style={{ padding: 20 }}>
-        <h3>Mi perfil</h3>
-        <p>Aquí verás tus publicaciones y podrás editar tu nombre.</p>
+        <h3>Tu perfil (@{user.username})</h3>
+
+        
+        <MyUserProfile/>
+
+        <h4>Tus publicaciones</h4>
+        <MyPublications/>
       </main>
-      <MyPublication/>
     </>
   );
 }
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,22 +1,22 @@
-// src/pages/ProfilePage.jsx
 import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import ProfilePublications from "../components/ProfilePublications";
-
-
-
+import UserProfile from "../components/UserProfile";
 
 export default function ProfilePage() {
   const { name } = useParams();
+
   return (
     <>
       <Header />
       <main style={{ padding: 20 }}>
         <h3>Perfil de {name}</h3>
-        <p>Aquí se mostrarán sus publicaciones.</p>
-        <ProfilePublications/>
+        <UserProfile/>
+        <h4>Publicaciones</h4>
+        <ProfilePublications />
       </main>
     </>
   );
 }
+
 
